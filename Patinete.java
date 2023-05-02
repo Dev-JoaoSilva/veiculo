cbf024f3a54e8f6c7fefbb422f350f770e3ffc3a
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-05-02 15:04:01 +0000

    salvar

---
+++ Patinete.java
@@ -0,0 +1,10 @@
+public class Patinete extends Veiculo  {
+    
+    public Patinete (String nome, String marca, String cor, int ano){
+        super(nome, marca, cor, ano);
+    }
+    public Patinete(){
+        
+    }  
+}
+
